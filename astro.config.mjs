69e726926cf8cc3dc3101f78ba940a7426ccce8b
Frank Stallone <frank@designzen.co>
2024-04-01 07:23:57 -0400

    feat: ✨ Adding GA in a service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), partytown(), sitemap()]
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  }), sitemap()]
 });
